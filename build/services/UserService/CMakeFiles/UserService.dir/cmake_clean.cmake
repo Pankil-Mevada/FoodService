42ed5d8027fdfe8631ab168a6026d8be39c24120
Pankil-Mevada <114727069+Pankil-Mevada@users.noreply.github.com>
2026-07-02 06:09:07 +0000

    Initial project structure and UserService setup

---
+++ build/services/UserService/CMakeFiles/UserService.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UserService.dir/src/main.cpp.o"
+  "CMakeFiles/UserService.dir/src/main.cpp.o.d"
+  "UserService"
+  "UserService.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UserService.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
